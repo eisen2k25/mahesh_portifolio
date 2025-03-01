bf32109a64b7a3e48662f87117d35799728e54f2
maheshdevu <maheshdevu17@gmail.com>
2025-03-01 10:15:43 +0530

    first commit

--- src/content_option.js
+++ src/content_option.js
@@ -1,144 +1,117 @@
-const logotext = "JOHN";
+const logotext = "Mahi";
 const meta = {
-    title: "John Doe",
+    title: "Mahesh Devu",
     description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Mahesh Devu",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I develop ML Models",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "I'm Mahesh, a versatile tech enthusiast with a strong commitment to knowledge sharing and innovation. where I focus on coding, MERN Stack development, and engaging in in-depth discussions about the ever-evolving tech landscape. My genuine love for AI adds an extra layer of excitement to my tech journey.",
+    your_img_url: "https://res.cloudinary.com/dmw5kp4tr/image/upload/v1705644685/profile_pic_s0zfbv.jpg",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "At NXT Wave's CCBP 4.0 Academy, I’m currently learning full-stack development with a specialization in 4.0 technologies. In love with the coding challenges and hands-on projects. With Industry’s first ever IRC 4.0, getting ready to take on new challenges in the tech world.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Data analysis Intern",
+        where: "Cognifyz Technologies",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Project Analysis Intern",
+        where: "VISAKHAPATNAM STEEL PLANT",
+        date: "2024(JUNE)",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Data science and machine learning Intern",
+        where: "BRAINOVISION SOLUTIONS INDIA PVT.LTD",
+        date: "2024",
     },
 ];
 
 const skills = [{
         name: "Python",
-        value: 90,
+        value: 80,
     },
     {
-        name: "Djano",
+        name: "HTML",
         value: 85,
     },
     {
         name: "Javascript",
-        value: 80,
+        value: 70,
     },
     {
         name: "React",
         value: 60,
     },
     {
-        name: "Jquery",
+        name: "DBMS",
         value: 85,
     },
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Python Developer",
+        description: "The most modern and high-quality Programming language made at a professional level.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Web development",
+        description: "High-quality development of sites at the professional level.  ",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Data Science",
+        description: "Things get done only if the data we gather can inform and inspire those in a position to make a difference.",
+    },
+    {
+        title: "Machine learning",
+        description: "This technology allows us to collect or produce data output from experience.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dmw5kp4tr/image/upload/c_crop,g_auto,h_800,w_800/ujzi9kop02h6zx2i9zev",
+        description: "EISEN2K25 is a React-powered website for the Metallurgy fest, featuring event details, registration, schedules, and interactive updates. ",
+        link: "https://www.eisen2k25.online/",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dkuriblkh/image/upload/v1720720482/Screenshot_2024-07-11_231126_kkjmmq.png",
+        description: "Food Munch Website",
+        link: "https://thefood17.ccbp.tech/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dkuriblkh/image/upload/v1720720478/Screenshot_2024-07-11_230147_zvc0g4.png",
+        description: "Tourism Website",
+        link: "https://tourismweb17.ccbp.tech/",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dkuriblkh/image/upload/v1720720473/Screenshot_2024-07-11_230056_ad6mvs.png",
+        description: "Todo List",
+        link: "https://todoproject17.ccbp.tech/",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dkuriblkh/image/upload/v1720720477/Screenshot_2024-07-11_231333_vwahey.png",
+        description: "Personal Portifolio",
+        link: "https://todoproject17.ccbp.tech/",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dkuriblkh/image/upload/v1720721226/Screenshot_2024-07-11_233649_g9ftlc.png",
+        description: "Gen AI",
+        link: "https://mygenai17.ccbp.tech/",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "maheshdevu03@gmail.com",
+    YOUR_FONE: "+91 1234567890",
+    description: "I'm always eager to connect with fellow tech enthusiasts, creators, and professionals who share my passion for technology, learning, and growth. Let's connect and explore the limitless possibilities the tech world has to offer. Together, we can inspire, innovate, and make a lasting impact. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,10 +120,10 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/maheshdevu110",
+    instagram: "https://www.instagram.com/the_mahesh_13/",
+    linkedin: "https://www.linkedin.com/in/mahesh-devu-684b40252/",
+    twitter: "https://x.com/Maheshdevu6",
 };
 export {
     meta,
